84458b71c0a8f7b1a719a9780c94a12d45f0d3c3
Jiaming927 <masterjm@cs.washington.edu>
2014-07-03 15:55:29 +0800

    Bad encoding still, may switch website

--- TaobaoOdds.py
+++ TaobaoOdds.py
@@ -1,22 +1,24 @@
 from bs4 import BeautifulSoup
 import urllib
 
-url = urllib.urlopen("http://c.caipiao.taobao.com/lottery/order/lottery_jczq_hhgg_by_client.htm?spm=a2126.6843133.1151804389.d4912993.YJTbx5")
+url = urllib.urlopen("http://c.caipiao.taobao.com/lottery/order/lottery_jczq_hhgg_by_client.htm")
 
 content = url.read()
 
-# soup = BeautifulSoup(content, fromEncoding=”GB18030“)
-soup = BeautifulSoup(content);
+soup = BeautifulSoup(content)
 
-# Home team
-# Visit team
+# td .Home team => a .h => title
 
 
-for li in soup.originalEncoding.find_all("li", {"class" : "home betli"}):
+for a in soup.find_all("a", {"class" : "h"}):
+	if a.parent.name == "td":
+		print a["title"].encode('GBK') 
+
+for li in soup.find_all("li", {"class" : "home betli"}):
 	print li.getText();
 
-for li in soup.originalEncoding.find_all("li", {"class" : "deuce betli"}):
+for li in soup.find_all("li", {"class" : "deuce betli"}):
 	print li.getText();
 
-for li in soup.originalEncoding.find_all("li", {"class" : "visit betli exp"}):
+for li in soup.find_all("li", {"class" : "visit betli exp"}):
 	print li.getText();
