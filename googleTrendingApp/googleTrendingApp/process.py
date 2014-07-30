843e418e1e64d88e69ae5515ecd702dd8de0b4ed
Jiaming927 <masterjm@cs.washington.edu>
2014-07-30 23:55:56 +0800

    Update

--- googleTrendingApp/googleTrendingApp/process.py
+++ googleTrendingApp/googleTrendingApp/process.py
@@ -1,6 +1,12 @@
 import json
 from glob import glob
 
+# This code will grab all the items*.json file 
+# And do something with it
+# Preparing to put this on a server and run this periodically
+# Then it can use the data to plot the difference
+
+
 files = glob('items*.json')
 
 apps = []
