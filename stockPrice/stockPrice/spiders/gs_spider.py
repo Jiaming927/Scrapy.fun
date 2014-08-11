6f5cd2ca53679901a75395fd6340ab822bbdb887
Jiaming927 <masterjm@cs.washington.edu>
2014-08-11 23:50:28 +0800

    had to change plan
    google finance uses ajax to fetch data, so I will need to either teach
    the spider to wait or try the other approach

--- stockPrice/stockPrice/spiders/gs_spider.py
+++ stockPrice/stockPrice/spiders/gs_spider.py
@@ -5,14 +5,5 @@ from stockPrice.items import StockPriceItem
 class GSSpider(Spider):
 	name = "googleStock"
 	allowed_domains = ["google.com"]
-	start_urls = ["https://www.google.com/finance/portfolio?action=view&pid=1&ei=fKXkU_DaG-T2wAO8wIC4BA"]
-	print 'hahaha'
-	print ''
+	start_urls = [""]
 	def parse(self, response):
-		print response
-		# sel = Selector(response)
-		# items = []
-		# for stock in sel.xpath('//tr/td[@class="pf-table-lname pf-table-cell lft"]'):
-		# 	name = stock.xpath('a/text()').extract()
-		# 	print 'wassup'
-		#return items
