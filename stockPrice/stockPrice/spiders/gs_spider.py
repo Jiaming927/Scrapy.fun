e69d6e71b0e9869478872f941a5e2696ff252022
Jiaming927 <masterjm@cs.washington.edu>
2014-08-12 23:57:04 +0800

    change of plan
    So I can’t get the content because it uses ajax to get the prices, but
    I really like this idea, so I switch to Yahoo’s api. This is not
    exactly a scrapy project anymore. But I decided to put this here. :)

--- stockPrice/stockPrice/spiders/gs_spider.py
+++
@@ -1,9 +0,0 @@
-from scrapy.spider import Spider
-from scrapy.selector import Selector
-from stockPrice.items import StockPriceItem
-
-class GSSpider(Spider):
-	name = "googleStock"
-	allowed_domains = ["google.com"]
-	start_urls = [""]
-	def parse(self, response):
