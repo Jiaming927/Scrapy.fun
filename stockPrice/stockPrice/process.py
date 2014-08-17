c59014809ddc02fb401ce5e78497c31f2fc2d340
Jiaming927 <masterjm@cs.washington.edu>
2014-08-17 15:55:38 +0800

    More data, more codes

--- stockPrice/stockPrice/process.py
+++ stockPrice/stockPrice/process.py
@@ -1,15 +1,14 @@
 import urllib, json, csv
 
-def getData(stock):
-	url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22' + stock + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
-	response = urllib.urlopen(url);
-	data = json.loads(response.read())
-	print 'Ask: ' + str(data['query']['results']['quote']['Ask'])
-	print 'Change:' + str(data['query']['results']['quote']['Change_PercentChange'])
-
-
+leftUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22'
+rightUrl = '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
 with open('list.csv', 'rU') as csvFile:
 	reader = csv.reader(csvFile)
 	for row in reader:
-		print 'Getting ' + row[0]
-		getData(row[1])
+		response = urllib.urlopen(leftUrl + row[1] + rightUrl)
+		data = json.loads(response.read())
+		with open(row[1] + '.json', 'w') as outfile:
+  			json.dump(data['query']['results'], outfile)
+
+# print 'Ask: ' + str(data['query']['results']['quote']['Ask'])
+# print 'Change:' + str(data['query']['results']['quote']['Change_PercentChange'])
--- stockPrice/stockPrice/spiders/gs_spider.py
+++ stockPrice/stockPrice/spiders/gs_spider.py
@@ -1,13 +1,16 @@
-import csv, os
+import csv, os, json
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 from stockPrice.items import StockPriceItem
+
+# Daily email
+# Alert email
 
 class GSSpider(Spider):
 	def generateList():
 		print os.getcwd()
 		result = []
-		url = "https://www.google.com/finance?q="
+		url = "http://finance.yahoo.com/q?s="
 		with open('stockPrice/list.csv', 'rU') as csvFile:
 			reader = csv.reader(csvFile)
 			for row in reader:
@@ -15,16 +18,33 @@ class GSSpider(Spider):
 			return result
 
 	name = "googleStock"
-	allowed_domains = ["google.com"]
+	allowed_domains = ["yahoo.com"]
 	start_urls = generateList()
 
 	def parse(self, response):
 		sel = Selector(response)
-		# elt = sel.xpath('//div[@id="price-panel"]/div/span/span/text()')
+		name = str(sel.xpath('//div[@class="title"]/h2/text()').extract())
+		elt = sel.xpath('//div[@class="yfi_rt_quote_summary_rt_top sigfig_promo_1"]/div/span/span/text()')
+		time = elt[7].extract()
+		price = elt[0].extract()
+		change = elt[2].extract()
+
+		left = name.index('(')
+		right = name.index(')')
+		name = name[left + 1:right].strip()
+
+		with open('stockPrice/' + name + '.json', 'rU') as jsonFile:
+			data = json.load(jsonFile)
+			print name
+			print int(data['quote']['AverageDailyVolume']) - int(data['quote']['Volume'])
+			#YearLow
+
+
+		# elt = sel.xpath('//table[@id="table2"]')
 		# time = elt[1].extract()
 		# price = elt[0].extract()
 
-		# elt = sel.xpath('//table[@class="snap-data"]/tbody/tr/td[@class="val"]')
+		#elt = sel.xpath('//table[@class="snap-data"]/tbody/tr/td[@class="val"]')
 		# volume = elt[3]
 		# Range = elt[0].extract()
 
